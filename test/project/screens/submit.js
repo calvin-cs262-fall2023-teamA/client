46d9322acdc71519f19c2e58e0e6163642a53526
Branden21 <statefan1324@gmail>
2023-10-02 18:39:21 -0400

    test for pop-up screen

---
+++ test/project/screens/submit.js
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+import { Button, View, Text, TouchableOpacity, FlatList } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+
+export default function SubmitApp() {
+    <Button label="Submit" onPress={() => setShowAppOptions(true)} />
+  }
