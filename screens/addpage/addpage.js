301f6d399ec31d678d8a78f6a3cb6b11669297d8
Steven M <smm56@calvin.edu>
2023-10-21 14:45:18 -0400

    add page form. does not send information to other pages or to database

--- screens/addpage/addpage.js
+++ screens/addpage/addpage.js
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
-import { Button, View, Text, TouchableOpacity, FlatList, TextInput, StyleSheet } from 'react-native';
+import { Button, View, Text, TouchableOpacity, Switch, StyleSheet } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import MyTextInput from './text';
 import ImageViewer from '../components/ImageViewer';
 import * as ImagePicker from 'expo-image-picker';
 import ImageButton from '../components/Buttons';
 import InputField from '../components/InputField';
+import DropDownPicker from 'react-native-dropdown-picker';
 
 function AddPage({ route }) {
   const navigation = useNavigation(); //used for navigation.navigate()
@@ -13,10 +14,32 @@ function AddPage({ route }) {
   //information entered by the user that needs to be sent to the database for an Item.
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
-  const [category, setCategory] = useState(""); //may need several, one for now until we figure out what those should be.
+  
+
+  //Main categories. May have subcategories in a different dropdown/selection tool (color? etc.) later.
+  const [categories, setCategories] = useState([
+    {label: 'Books/Notebooks', value: 'books'},
+    {label: 'Clothing/Accessories', value: 'clothing'},
+    {label: 'Jewelry', value: 'jewelry'},
+    {label: 'Electronics', value: 'electronics'},
+    {label: 'Keys', value: 'keys'},
+    {label: 'Personal Items (Umbrella, Water Bottle, etc.)', value: 'items'},
+
+    {label: 'Other', value: 'other'}, //catch-all
+  ]); 
+  const [value, setValue] = useState(null); //value stored in dropdown (see categories item label/value)
+  const [open, setOpen] = useState(false); //handles user clicking on dropdown. Opens/closes the dropdown menu.
+
   const [location, setLocation] = useState(null);
   //status = resolved or unresolved. Not entered when creating the card.
   const [lostorfound, setLostOrFound] = useState("lost") //either lost or found. A string for now but could be a boolean.
+  //for Switch (selecting lost/found)
+  const [isEnabled, setIsEnabled] = useState(false);
+  const toggleSwitch = () => {
+    setIsEnabled(previousState => !previousState); 
+    isEnabled ? setLostOrFound("found") : setLostOrFound("lost");
+  }
+
   //image handled below
 
   const PlaceholderImage = require('../../assets/icon.png');
@@ -40,21 +63,48 @@ function AddPage({ route }) {
     <View style={{ flex: 1, padding: 20 }}>
 
       <TouchableOpacity style={styles.imageContainer} onPress={pickImageAsync}>
-        {/*make this work like a button press then remove the actuall button (or put placeholder text on the placeholder image)? Refer to figma design.*/}
         <ImageViewer
           placeholderImageSource={PlaceholderImage}
           selectedImage={selectedImage}
-          onPress={pickImageAsync}
+          onPress={pickImageAsync} //click on image to modify. Should probably *change the default to make it more apparent that you can modify/upload images.
         />
       </TouchableOpacity>
 
       {/* A list of options for what kinds of things the user can add 
         (an item they lost or something they found). */}
       <View style={styles.inputContainer}>
+        <View style={styles.switchContainer}>
+          {/* Text and a switch */}
+          <Text style={!isEnabled ? styles.selectText : styles.unselectText}>I Lost...</Text>
+          <Switch
+            onValueChange={toggleSwitch}
+            value={isEnabled}
+          />
+          <Text style={isEnabled ? styles.selectText : styles.unselectText}>I Found...</Text>
+        </View>
         <InputField header="Title" bodySize={50} changeText={setName} />
         <InputField header="Description" bodySize={100} changeText={setDescription} />
-          {/* change to a dropdown later. */}
-        <InputField header="Category" bodySize={50} changeText={setCategory} />
+          {/* change to a dropdown later.         <InputField header="Category" bodySize={50} changeText={setCategory} /> */}
+        <DropDownPicker 
+          style={styles.dropdown}
+          items={categories}
+          value={value}
+          open={open}
+          setOpen={setOpen}
+          setValue={setValue}
+          setItems={setCategories}
+
+          /* It would be great if it was more apparent that the user can scroll down through a list of categories.
+             My initial thought was to make the scroll bar always visible (instead of just while scrolling), but I
+             haven't gotten that to work yet. */
+             //possible props I could modify [vvv] to accomplish [^^^]
+          //containerProps={{
+            //the dropdown container (a 'View')
+          //}}
+          //dropDownContainerStyle={{
+          //}}
+        />
+        {/* Location Field */}
       </View>
        
       
@@ -80,8 +130,25 @@ const styles = StyleSheet.create({
     borderRadius: 18,
   },
   inputContainer: {
+    marginBottom: '25%',
+  },
+  switchContainer: {
+    flexDirection: 'row', 
+    justifyContent: 'space-between',
+    marginHorizontal: '10%',
+    alignItems: 'center',
+  },
+  selectText: {
+    fontWeight: 'bold',
+    color: '#DBB3E3',
+    fontSize: 24,
+  },
+  unselectText: {
+    color: '#00000099',
+  },
+  dropdown: {
     
-  }
+  },
 });
 
 export default AddPage;
