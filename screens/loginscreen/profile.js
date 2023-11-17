574ea048521d7f2c1926dfbb7adfc41c04ef0085
harryg02 <94257270+harryg02@users.noreply.github.com>
2023-11-17 15:16:54 -0500

    tried to fix profile page indent

--- screens/loginscreen/profile.js
+++ screens/loginscreen/profile.js
@@ -1,80 +1,76 @@
-import React, { useEffect, useState } from 'react';
-import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
-import * as ImagePicker from 'expo-image-picker';
-import ImageButton from '../components/Buttons';
-import ImageViewer from '../components/ImageViewer';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import React, { useEffect, useState } from 'react'
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
+import * as ImagePicker from 'expo-image-picker'
+// import ImageButton from '../components/Buttons'
+import ImageViewer from '../components/ImageViewer'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
+const Profile = ({}) => {
+  const navigation = useNavigation()
 
-const Profile = ({  }) => {
-  const navigation = useNavigation();
-
-  //image handled below
-
-  const PlaceholderImage = require('../../assets/user.png');
-  const [selectedImage, setSelectedImage] = useState(null);
+  //  image handled below
+  const PlaceholderImage = require('../../assets/user.png')
+  const [selectedImage, setSelectedImage] = useState(null)
   // const { userData } = useUser();
   // const { userID, userName } = userData;
-  const [email, setEmail] = useState('');
-  const [userID, setUserID] = useState('');
-  const [userName, setUsername] = useState('');
-  
+  const [email, setEmail] = useState('')
+  const [userID, setUserID] = useState('')
+  const [userName, setUsername] = useState('')
+
   useEffect(() => {
     // Retrieve user data from AsyncStorage
     const retrieveUserData = async () => {
-        try {
-            const userData = await AsyncStorage.getItem('userData');
-            if (userData) {
-                const { ID, userName, email, username, password } = JSON.parse(userData);
-                setUserID(ID)
-                setEmail(email);
-                setUsername(userName);
-            }
-        } catch (error) {
-            console.error(error);
+      try {
+        const userData = await AsyncStorage.getItem('userData')
+        if (userData) {
+          const { ID, userName, email, username, password } = JSON.parse(userData)
+          setUserID(ID)
+          setEmail(email)
+          setUsername(userName)
         }
-    };
-
+      } catch (error) {
+        console.error(error)
+      }
+    }
     retrieveUserData();
-}, []);
-  
+  }, [])
+
   const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
       allowsEditing: true,
-      quality: 1,
-    });
+      quality: 1
+    })
 
     if (!result.canceled) {
       setSelectedImage(result.assets[0].uri);
     } else {
       alert('You did not select any image.');
-      
     }
   }
 
   const handleLogout = async () => {
     try {
       // Clear all stored data in AsyncStorage
-      await AsyncStorage.clear();
+      await AsyncStorage.clear()
       // Navigate to the login page
-      navigation.navigate('Login', { prevRoute: 'Login' });
+      navigation.navigate('Login', { prevRoute: 'Login' })
     } catch (error) {
-      console.error('Error during logout:', error);
+      console.error('Error during logout:', error)
     }
-  };
-  
+  }
+
   return (
     <View style={styles.container}>
       <TouchableOpacity onPress={pickImageAsync}>
         <ImageViewer
           placeholderImageSource={PlaceholderImage}
           selectedImage={selectedImage}
-          onPress={pickImageAsync} //click on image to modify.
+          onPress={pickImageAsync} //  click on image to modify.
           style={styles.ImageViewerStyle}
         />
       </TouchableOpacity>
-      
+
       <Text style={styles.userName}>{userName}</Text>
       <Text style={styles.userEmail}>{email}</Text>
 
@@ -102,24 +98,24 @@ const Profile = ({  }) => {
       </TouchableOpacity>
 
     </View>
-  );
+  )
 }
 
-export default Profile;
+export default Profile
 
 const styles = StyleSheet.create({
-  
+
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#EDE7E7',
-    padding: 35,
+    padding: 35
   },
   ImageViewerStyle: {
     width: 150,
     height: 150,
-    borderRadius: 200,
+    borderRadius: 200
   },
   userName: {
     flexDirection: 'row',
@@ -127,20 +123,20 @@ const styles = StyleSheet.create({
     fontWeight: '900',
     paddingTop: 16,
     fontSize: 30,
-    color: '#2F2E41',
+    color: '#2F2E41'
   },
   userEmail: {
     marginBottom: 8,
     color: '#2F2E41',
     fontSize: 18,
     fontWeight: 'bold',
-    marginTop: 3,
+    marginTop: 3
   },
   flexContainer: {
     marginTop: 80,
     marginBottom: 80,
     flexDirection: 'row', // Arrange elements horizontally
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
 
   lostNFound: {
@@ -149,10 +145,10 @@ const styles = StyleSheet.create({
     alignItems: 'center', // Center text horizontally
     backgroundColor: 'fff',
     shadowColor: '#A59D95',
-    shadowOffset: {width: 0, height: 8},
+    shadowOffset: { width: 0, height: 8 },
     shadowOpacity: 0.2,
     shadowRadius: 24,
-    elevation: 7,     //drop-shadow(0px 8px 24px rgba(165, 157, 149, 0.20)),
+    elevation: 7 //  drop-shadow(0px 8px 24px rgba(165, 157, 149, 0.20)),
   },
 
   primaryButton: {
@@ -164,33 +160,32 @@ const styles = StyleSheet.create({
     marginBottom: 10,
     marginTop: 30,
     shadowColor: '#A59D95',
-    shadowOffset: {width: 0, height: 8},
+    shadowOffset: { width: 0, height: 8 },
     shadowOpacity: 0.2,
     shadowRadius: 24,
-    elevation: 7,     //drop-shadow(0px 8px 24px rgba(165, 157, 149, 0.20)),
-    zIndex: -1,
+    elevation: 7, //  drop-shadow(0px 8px 24px rgba(165, 157, 149, 0.20)),
+    zIndex: -1
   },
 
   primaryButtonText: {
     color: '#342F2F',
     fontWeight: '900',
-    fontSize: 20,
-    
+    fontSize: 20
   },
 
   secondaryButton: {
     alignItems: 'center',
-    //backgroundColor: '#FAF2F2',
+    //  backgroundColor: '#FAF2F2',
     borderRadius: 50,
     width: '85%',
     padding: 18,
     marginBottom: 10,
-    marginTop: 10,
+    marginTop: 10
   },
   secondaryButtonText: {
     color: '#9E8B8D',
     fontWeight: '900',
-    fontSize: 20,
+    fontSize: 20
   },
 
   tertiaryButton: {
@@ -202,16 +197,16 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     marginHorizontal: 14,
     shadowColor: '#A59D95',
-    shadowOffset: {width: 0, height: 8},
+    shadowOffset: { width: 0, height: 8 },
     shadowOpacity: 0.2,
     shadowRadius: 24,
-    elevation: 7,     //drop-shadow(0px 8px 24px rgba(165, 157, 149, 0.20)),
+    elevation: 7 // drop-shadow(0px 8px 24px rgba(165, 157, 149, 0.20)),
   },
 
   tertiaryButtonTitle: {
     color: '#342F2F',
     fontWeight: '900',
-    fontSize: 50,
+    fontSize: 50
   },
 
   tertiaryButtonText: {
@@ -219,7 +214,6 @@ const styles = StyleSheet.create({
     fontWeight: '900',
     fontSize: 18,
     marginTop: -6,
-    marginBottom: 6,
-  },
-});
-
+    marginBottom: 6
+  }
+})
