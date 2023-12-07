5eb4bfc7d4b975806144d42d9415371dd5620c1e
harryg02 <94257270+harryg02@users.noreply.github.com>
2023-12-07 12:15:20 -0500

    minor styling change

--- styles/MainPageStyles.js
+++ styles/MainPageStyles.js
@@ -84,7 +84,7 @@ const styles = StyleSheet.create({
     flex: 1,
     // backgroundColor: '#EDE7E7',
     position: 'absolute',
-    bottom: 0,
+    bottom: -20,
     paddingTop: 10,
     width: '100%',
     // height: 100,
